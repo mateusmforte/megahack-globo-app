35519b64517fd7e4f8c7093a2d0633e735d21a1c
Mateus Moura Forte <m-forte@hotmail.com>
2020-05-02 23:59:49 -0300

    WIP Configurando o visual da navbar

---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Navigation.scss";
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <a href="#" className="navigation__link g1 disabled"></a>
+      <a href="#" className="navigation__link ge disabled"></a>
+      <a href="#" className="navigation__link feed disabled"></a>
+      <a href="#" className="navigation__link gshow disabled"></a>
+      <a href="#" className="navigation__link globoplay disabled"></a>
+    </div>
+  );
+};
+
+export default Navigation;
