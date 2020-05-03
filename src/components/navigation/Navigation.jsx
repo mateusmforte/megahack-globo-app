48ecd6c6199d1d40434d09562c5db04a548a5dbb
Mateus Moura Forte <m-forte@hotmail.com>
2020-05-03 11:28:17 -0300

    Adicionando os visuais active e ajustes no tamanho e responsividade

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -3,11 +3,11 @@ import "./Navigation.scss";
 const Navigation = () => {
   return (
     <div className="navigation">
-      <a href="#" className="navigation__link g1 disabled"></a>
-      <a href="#" className="navigation__link ge disabled"></a>
-      <a href="#" className="navigation__link feed disabled"></a>
-      <a href="#" className="navigation__link gshow disabled"></a>
-      <a href="#" className="navigation__link globoplay disabled"></a>
+      <a href="#" className="navigation__link g1 active"></a>
+      <a href="#" className="navigation__link ge "></a>
+      <a href="#" className="navigation__link feed "></a>
+      <a href="#" className="navigation__link gshow "></a>
+      <a href="#" className="navigation__link globoplay "></a>
     </div>
   );
 };
