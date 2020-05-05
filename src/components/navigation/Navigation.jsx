357cbb37c11dc5a376ca78b4d58ebc8974316da5
Mateus Moura Forte <m-forte@hotmail.com>
2020-05-04 21:01:42 -0300

    Mudança dos logos e splash screen

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -3,9 +3,9 @@ import "./Navigation.scss";
 const Navigation = () => {
   return (
     <div className="navigation">
-      <a href="#" className="navigation__link g1 active"></a>
+      <a href="#" className="navigation__link g1 "></a>
       <a href="#" className="navigation__link ge "></a>
-      <a href="#" className="navigation__link feed "></a>
+      <a href="#" className="navigation__link feed active"></a>
       <a href="#" className="navigation__link gshow "></a>
       <a href="#" className="navigation__link globoplay "></a>
     </div>
